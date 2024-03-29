bcb5b472434b78eb329cd6d2092c29dbf27eb915
vungocthy <ngocthyvu1107@gmail.com>
2024-03-29 23:56:36 +0700

    build again

--- src/api/order-api.js
+++ src/api/order-api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { BASE_URL, CURRENT_USER } from "../libs/constants";
-import axios from "axios";
 
 export async function GetOrderByStoreId(param) {
   let STOREID = CURRENT_USER.user.storeId;
