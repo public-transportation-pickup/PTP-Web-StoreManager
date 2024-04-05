18d90ca1bd5ff05f304bbefb7c5fdf1a8c6f95cd
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-04-05 18:27:15 +0700

    Edit

--- src/components/shared/Note.jsx
+++ src/components/shared/Note.jsx
@@ -38,7 +38,7 @@ export default function Note({button,id,noteStringFunc}) {
           <button
             type="button"
             onClick={openModal}
-            className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+            className="rounded-md bg-black/20 px-4 py-2 w-fit text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
           >
             {button}
           </button>
