472256f49fcdab349f9b1cb8bfc0e2bf2a3655fb
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-04-13 03:21:31 +0700

    Edit

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -1,10 +1,13 @@
 import { Outlet, useLocation } from "react-router-dom";
 
 // components
+import Connector from "../libs/constants/signalr-connection.ts";
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState,useEffect } from "react";
 
 import AdminNavbar from "../components/Navbars/AdminNavbar";
 import Sidebar from "../components/Sidebar/Sidebar";
-import HeaderStats from "../components/Headers/HeaderStats";
 //import FooterAdmin from "../components/Footers/FooterAdmin";
 
 // views
@@ -16,8 +19,25 @@ import HeaderStats from "../components/Headers/HeaderStats";
 
 function Admin() {
   const {pathname}=useLocation();
-  
+   //#region SignalR
+   var CURRENT_USER = JSON.parse(localStorage.getItem("user"));
+   let STOREID = CURRENT_USER.user.storeId;
+   const { newMessage, events } = Connector();
+  //  const [message, setMessage] = useState("");
+   useEffect(() => {
+       events((username, message) => {
+           if(username==="CreateOrder" && message===STOREID){
+            // setMessage(message)
+            toast("Bạn có đơn hàng mới trong hàng chờ!",{autoClose:2000});
+           }
+       });
+   },[]);
+
+
+   //#endregion
   return (
+    <>
+    {/* <ToastContainer className="w-100 h-10"/> */}
     <div className="flex flex-row">
       <div><Sidebar /></div>
       <div className=" h-full w-screen md:ml-56 bg-[#f7f7ff]">
@@ -29,6 +49,7 @@ function Admin() {
         </div>
       </div>
     </div>
+    </>
   );
 }
 export default Admin;
